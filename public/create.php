de8b390c0ff7a46f1b60331cdc2b4acb0dadf253
Dylan <dylanhrebenach@gmail.com>
2019-03-21 19:12:57 -0700

    added read.php and ability to look up users by email

--- public/create.php
+++ public/create.php
@@ -1,13 +1,53 @@
-<?php include "templates/header.php"; ?><h2>Add a user</h2>
+<?php
+
+if (isset($_POST['submit'])) {
+  require "../config.php";
+  require "../common.php";
+
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+
+$new_user = array(
+  "firstName" => $_POST['firstName'],
+  "lastName"  => $_POST['lastName'],
+  "email"     => $_POST['email'],
+  "password"  => $_POST['password']
+);
+
+$sql = sprintf(
+    "INSERT INTO %s (%s) values (%s)",
+    "users",
+    implode(", ", array_keys($new_user)),
+    ":" . implode(", :", array_keys($new_user))
+);
+
+$statement = $connection->prepare($sql);
+$statement->execute($new_user);
+
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+  }
+
+}
+
+?>
+
+<?php include "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+  <?php echo escape($_POST['firstName']); ?> successfully added.
+<?php } ?>
+
+<h2>Add a user</h2>
 
 <form method="post">
     <div class="input-group">
         <label for="firstname">First Name</label>
-        <input type="text" name="firstName" id="firstname">
+        <input type="text" name="firstName" id="firstName">
     </div>
     <div class="input-group">
         <label for="lastname">Last Name</label>
-        <input type="text" name="lastName" id="lastname">
+        <input type="text" name="lastName" id="lastName">
     </div>
     <div class="input-group">
         <label for="email">Email Address</label>
